73c269932068dac0b210909ad3037db8f9acaa7a
Rayyan Quraishi <5747484+rayyanQ@users.noreply.github.com>
2024-07-01 17:50:11 -0400

    remove duplicate page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,7 +4,7 @@ import Tiptap from "@/components/tiptap";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Tiptap />
+      placeholder
     </main>
   );
 }
