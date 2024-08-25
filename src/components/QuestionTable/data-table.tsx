c6d2edf28e9e7a80f4358454f147f2623c1033a7
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-08-25 12:19:44 -0400

    add lists to the tiptap editor

--- src/components/QuestionTable/data-table.tsx
+++ src/components/QuestionTable/data-table.tsx
@@ -138,16 +138,16 @@ export function DataTable<TData, TValue>({
           <TableBody>
             {table.getRowModel().rows?.length ? (
               table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </TableCell>
-                  ))}
-                </TableRow>
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </TableCell>
+                    ))}
+                  </TableRow>
               ))
             ) : (
               <TableRow>
--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -1,18 +1,15 @@
 'use client'
 
-import { Bold, Italic, Underline as UnderlineIcon } from "lucide-react"
- 
 import {
-  ToggleGroup,
-  ToggleGroupItem,
-} from "@/components/ui/toggle-group"
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { useEditor, EditorContent, Editor } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import Underline from '@tiptap/extension-underline';
-import Mathematics from '@tiptap-pro/extension-mathematics'
+  Bold,
+  Italic,
+  UnderlineIcon,
+  ListIcon,
+  ListOrderedIcon,
+  UndoIcon,
+  RedoIcon,
+} from "lucide-react"
 
-import 'katex/dist/katex.min.css'
 import {
   Select,
   SelectContent,
@@ -22,67 +19,104 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Button } from "@/components/ui/button"
+import { useEditor, EditorContent, Editor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import Underline from '@tiptap/extension-underline';
+import Mathematics from '@tiptap-pro/extension-mathematics'
 
-/**
- * 
- * "@tiptap/pm": "^2.4.0",
- * "@tiptap/react": "^2.4.0",
- * "@tiptap/starter-kit - bold
- *  .configure({
-        heading: false,
-        codeBlock: false,
-        underline: false,
-        italic: false,
-        strike: false,
-        listItem: false,
-        orderedList: false,
-        bulletList: false,
-        blockquote: false,
-        hardBreak: false,
-        horizontalRule: false,
-        gapcursor: false,
-      }),
- * "@tiptap-pro/extension-mathematics": "^2.10.7",
- * "@tiptap/extension-underline": "^2.6.4",
- * 
- */
+import { useState } from 'react'
+
+import 'katex/dist/katex.min.css'
+
 const Tiptap = () => {
 
-  /**
-   * TODO:
-   * - add a button to insert a math block
-   * - add a button to insert images
-   * - add a button to insert tables
-   * - add gap cursor
-   * - put editor in focus, when user clicks on the editor box
-   */
   const editor = useEditor({
     extensions: [
-      StarterKit,
+      StarterKit.configure({
+        bulletList: {
+          HTMLAttributes: {
+            class: 'list-disc pl-6',
+          },
+        },
+        orderedList: {
+          HTMLAttributes: {
+            class: 'list-decimal pl-6',
+          },
+        },
+      }),
       Mathematics,
       Underline,
     ],
     content: '<p>Hello World! 🌎️</p>',
+    autofocus: 'all',
+    editorProps: {
+      attributes: {
+        class: 'w-full focus:outline-none',
+      },
+    },
   })
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-start">
       <MenuBar editor={editor} />
       <ScrollArea className="w-full h-full border rounded p-1">
-        <EditorContent editor={editor} className="w-full h-fit m-0" />
+        <EditorContent editor={editor} />
       </ScrollArea>
     </div>
   )
 }
 
 const MenuBar = ({ editor }: {editor: Editor|null}) => {
+  const [value, setValue] = useState("");
+
   if (!editor) {
     return null
   }
 
+  const setTextStyle = (value: string) => {
+    switch (value) {
+      case "Heading 1":
+        editor.chain().focus().setHeading({ level: 1 }).run()
+        break
+      case "Heading 2":
+        editor.chain().focus().setHeading({ level: 2 }).run()
+        break
+      case "Heading 3":
+        editor.chain().focus().setHeading({ level: 3 }).run()
+        break
+      case "Paragraph":
+        editor.chain().focus().setParagraph().run()
+        break
+      default:
+        break
+    }
+    setValue(value)
+  }
+  
   return (
-    <div className="flex flex-row justify-between w-full border rounded p-1 mb-1">
-      <ToggleGroup type="multiple">
+    <div className="flex flex-row justify-start w-full border rounded gap-2 p-1 mb-1">
+
+      <Select value={value} onValueChange={setTextStyle}>
+        <SelectTrigger className="w-[150px]">
+          <SelectValue placeholder="Text Style">
+            {value ? value : "Text Style"}
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Text Style</SelectLabel>
+            <SelectItem value="Heading 1">Heading 1</SelectItem>
+            <SelectItem value="Heading 2">Heading 2</SelectItem>
+            <SelectItem value="Heading 3">Heading 3</SelectItem>
+            <SelectItem value="Paragraph">Paragraph</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+
+      <ToggleGroup type="multiple" variant="outline">
         <ToggleGroupItem value="bold" aria-label="Toggle bold" data-state={editor.isActive('bold') ? 'on' : 'off'} onClick={() => editor.chain().focus().toggleBold().run()}>
           <Bold className="h-4 w-4" />
         </ToggleGroupItem>
@@ -94,20 +128,23 @@ const MenuBar = ({ editor }: {editor: Editor|null}) => {
         </ToggleGroupItem>
       </ToggleGroup>
 
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Text Style" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Text Style</SelectLabel>
-            <SelectItem value="h1">Heading 1</SelectItem>
-            <SelectItem value="h2">Heading 2</SelectItem>
-            <SelectItem value="h3">Heading 3</SelectItem>
-            <SelectItem value="p">Paragraph</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+      <ToggleGroup type="multiple" variant="outline">
+        <ToggleGroupItem value="bulletList" aria-label="Toggle buttle list" data-state={editor.isActive('bulletList') ? 'on' : 'off'} onClick={() => editor.chain().focus().toggleBulletList().run()}>
+          <ListIcon className="h-4 w-4" />
+        </ToggleGroupItem>
+        <ToggleGroupItem value="orderedList" aria-label="Toggle ordered list" data-state={editor.isActive('orderedList') ? 'on' : 'off'} onClick={() => editor.chain().focus().toggleOrderedList().run()}>
+          <ListOrderedIcon className="h-4 w-4" />
+        </ToggleGroupItem>
+      </ToggleGroup>
+
+      <div className="flex items-center justify-center gap-1">
+        <Button variant="outline" size="icon" aria-label="Undo" onClick={() => editor.chain().focus().undo().run()}>
+          <UndoIcon className="h-4 w-4" />
+        </Button>
+        <Button variant="outline" size="icon" aria-label="Redo" onClick={() => editor.chain().focus().redo().run()}>
+          <RedoIcon className="h-4 w-4" />
+        </Button>
+      </div>
 
     </div>
   )
