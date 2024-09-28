77ff84493975e4e86251ff8a4a23632496104555
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-09-28 13:31:14 -0400

    add temp heading on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Tiptap from "@/components/tiptap";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Kortex Academy</h1>
       <Tiptap />
     </main>
   );
