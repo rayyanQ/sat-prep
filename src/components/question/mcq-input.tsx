3a64c3c09281af50aad81b1365b82fb234fb5b82
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-01 21:01:40 -0400

    change mcq option styling

--- src/components/question/mcq-input.tsx
+++ src/components/question/mcq-input.tsx
@@ -18,7 +18,7 @@ const MCQInput = ({options}: {options: OptionsType}) => {
 
 const MCQOption = ({title, value}: {title: string, value: string}) => {
   return (
-    <li className="w-full h-12 px-4 flex flex-row justify-start items-center rounded border border-neutral-950">
+    <li className="w-full h-12 px-4 flex flex-row justify-start items-center rounded border border-neutral-950 hover:border-primary cursor-pointer">
       <span className="flex justify-center items-center border-2 border-neutral-950 rounded-full size-6 mr-6 font-medium">
         {title}
       </span>
