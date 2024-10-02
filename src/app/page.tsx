761a7320ed3392f21ac4680b2d9374069a72cb8d
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-02 01:06:53 -0400

    create a landing page with a link to waitlist typeform

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import Image from "next/image";
-import Tiptap from "@/components/tiptap";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Kortex Academy</h1>
-      <Tiptap />
+    <main className="min-h-screen flex flex-col items-center justify-center py-20">
+      <div className="container">
+        <div className="w-full h-64 relative">
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-6xl text-center font-bold text-white z-10">
+            Repository of SAT Practice Questions
+          </h1>
+          <Image
+            src="/hero.jpg"
+            alt="Hero"
+            fill
+            objectFit="cover"
+          />
+        </div>
+        <div className="flex flex-col justify-start items-center py-10">
+          <a href="https://kdjukckera9.typeform.com/to/XRsjn1Y2" target="_blank">
+            <Button className="p-10"><h2 className="text-4xl font-bold">Join the waitlist</h2></Button>
+          </a>
+        </div>
+      </div>
     </main>
   );
 }
