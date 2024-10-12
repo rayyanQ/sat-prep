34b62b4e7e0b68f360c986aed6cab20f1ea856a5
Rayyan Ahmed <5747484+rayyanQ@users.noreply.github.com>
2024-10-12 17:43:30 -0400

    remove attempt counter from footer

--- src/components/question/footer.tsx
+++ src/components/question/footer.tsx
@@ -26,7 +26,7 @@ const QuestionFooter = (
     <footer className="bottom-0 flex flex-row justify-center items-center w-full border-t">
       <div className="container h-14 px-10 grid grid-cols-3">
         <div className="col-span-1 flex flex-row justify-start items-center">
-          <p>Attempt #</p>
+          <p>{/*Attempt #*/}</p>
         </div>
         <div className="col-span-1 flex flex-row justify-center items-center">
           <div className="bg-neutral-950 text-white px-3 py-1 rounded">Question {currentQuestion} of {questionCount}</div>
